4364de747472829fc400137a7afc22867c7ba376
Resul0316 <93797905+Resul0316@users.noreply.github.com>
2023-05-30 21:00:05 +0200

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,6 @@ import { Link, useNavigate } from "react-router-dom";
 import { logOut } from "../auth/firebase";
 import { AuthContext } from "../context/AuthContext";
 
-
 const Navbar = () => {
   const navigate = useNavigate();
   const { currentUser } = useContext(AuthContext);
